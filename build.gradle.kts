58179b8f52b4b4450c16fe72b065137784a0304a
Eduard Dev <eduardkiller@hotmail.com>
2020-11-22 17:32:40 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,9 @@ dependencies {
     api("net.eduard:sqlmanager:1.0-SNAPSHOT")
     api("net.eduard.abstraction:complete:1.0-SNAPSHOT")
     compileOnly(kotlin("stdlib"))
-    testImplementation("org.bukkit:spigot:1.8.9")
-    testImplementation("junit", "junit", "4.12")
-    testImplementation(kotlin("stdlib"))
+    testCompileOnly("org.bukkit:spigot:1.8.9")
+    testCompileOnly("junit", "junit", "4.12")
+    testCompileOnly(kotlin("stdlib"))
 
 }
 tasks.withType<ShadowJar>{
