428079fcd3fa3cd7234238e0cb758b1147af4ad9
Eduard Dev <eduardkiller@hotmail.com>
2022-03-25 19:53:34 -0300

    EduardAPI PluginSettings Tempo de backup minimo para 1hora

--- src/main/java/net/eduard/api/lib/plugin/PluginSettings.java
+++ src/main/java/net/eduard/api/lib/plugin/PluginSettings.java
@@ -6,28 +6,26 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PluginSettings {
-    public static long BACKUP_MIN_SECONDS = 60 * 10;
+    public static long BACKUP_MIN_SECONDS = 60 * 60;
     private boolean debug = true;
     private boolean autoSave = false;
-    public boolean isAutoSave() {
-        return autoSave;
-    }
     private long autoSaveSeconds = 60;
-    private long autoBackupSeconds = 60 * 60 * 3;
-    public long getAutoBackupSeconds() {
+    private long autoBackupSeconds = BACKUP_MIN_SECONDS;
+    private long lastBackup = System.currentTimeMillis();
+    private transient long lastSave = System.currentTimeMillis();
 
+    public long getAutoBackupSeconds() {
         if (autoBackupSeconds < BACKUP_MIN_SECONDS) {
             autoBackupSeconds = BACKUP_MIN_SECONDS;
         }
         return autoBackupSeconds;
     }
-
+    public boolean isAutoSave() {
+        return autoSave;
+    }
     public long getLastBackup() {
         return lastBackup;
     }
-
-    private long lastBackup = System.currentTimeMillis();
-    private transient long lastSave = System.currentTimeMillis();
 
     public boolean isDebug() {
         return debug;
