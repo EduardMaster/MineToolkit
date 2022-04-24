24f0a06b2f5c8f029a7f57cc44eb4dbebbb3dfb6
Eduard Dev <eduardkiller@hotmail.com>
2022-04-24 12:08:44 -0300

    EduardAPI Interface PlotSystem atualizada

--- src/main/java/net/eduard/api/server/PlotSystem.kt
+++ src/main/java/net/eduard/api/server/PlotSystem.kt
@@ -1,15 +1,22 @@
 package net.eduard.api.server
 
 import org.bukkit.Location
+import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
 import java.util.*
 
 interface PlotSystem : PluginSystem {
     fun isSamePlot(location: Location, locationTarget: Location) : Boolean
     fun getPlot(location : Location) : PlotData?
+    fun hasPlot(location : Location) : Boolean
+    fun getPlots(player : Player) : Collection<PlotData>
 
     interface PlotData{
         val x : Int
         val z : Int
-        val owner : UUID
+        val owner : OfflinePlayer
+        val owners : Collection<OfflinePlayer>
+        val members : Collection<OfflinePlayer>
+        fun transfer(newOwner : OfflinePlayer) : Boolean
     }
 }
